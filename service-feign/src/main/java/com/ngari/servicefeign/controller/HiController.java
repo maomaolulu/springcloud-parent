c854910799997aff970daa3b69cc0a3ecd11a3ed
maoly <maoly@ngarihealth.com>
2020-12-23 16:44:31 +0800

    feign

---
+++ service-feign/src/main/java/com/ngari/servicefeign/controller/HiController.java
@@ -0,0 +1,26 @@
+package com.ngari.servicefeign.controller;
+
+import com.ngari.servicefeign.feign.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HiController
+ * @Description
+ * @Author maoLy
+ * @Date 2020/12/23
+ **/
+@RestController
+public class HiController {
+
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne( name );
+    }
+}
