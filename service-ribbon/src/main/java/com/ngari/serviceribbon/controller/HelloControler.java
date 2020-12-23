88dc43cad20ef126a3e4923afc6e07f62c70a543
maoly <maoly@ngarihealth.com>
2020-12-23 15:01:25 +0800

    rest+ribbon

---
+++ service-ribbon/src/main/java/com/ngari/serviceribbon/controller/HelloControler.java
@@ -0,0 +1,25 @@
+package com.ngari.serviceribbon.controller;
+
+import com.ngari.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloControler
+ * @Description
+ * @Author maoLy
+ * @Date 2020/12/23
+ **/
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+}
---
+++ service-ribbon/src/main/java/com/ngari/serviceribbon/service/HelloService.java
@@ -0,0 +1,24 @@
+package com.ngari.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName HelloService
+ * @Description
+ * @Author maoLy
+ * @Date 2020/12/23
+ **/
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+
+}
